91ef405a724a0b1978ed37588d86f6f119eb0e79
joaolobao380 <victor_jv380@hotmail.com>
2020-09-29 10:48:12 -0300

    fix route

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -17,7 +17,7 @@ const Repository: React.FC = () => {
     <>
       <Header>
         <img src={logoImg} alt="Github Explorer" />
-        <Link to="/dashbord">
+        <Link to="/">
           <FiChevronLeft size={16} />
           Voltar
         </Link>
